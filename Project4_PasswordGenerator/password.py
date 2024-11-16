8ff8f5d521e9d07941e831e620ba243d43b5af13
Preksha Barjatya <140542863+Prekshabarjatya@users.noreply.github.com>
2024-11-16 18:07:09 +0530

    Update password.py

--- Project4_PasswordGenerator/password.py
+++ Project4_PasswordGenerator/password.py
@@ -1,13 +1,13 @@
 import secrets
 
-def generate_passphrase(num_words, wordlist_path='diceware.wordlist.asc'):
+def generate_passphrase(num_words, wordlist_path=r'diceware.wordlist.asc'):
+
     with open(wordlist_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()[2:7778]
         word_list = [line.split()[1] for line in lines]
 
-    words = [secrets.choice(word_list) for i in range(num_words)]
+    words = [secrets.choice(word_list) for _ in range(num_words)]
     return ' '.join(words)
-
+n = int(input("Enter the number of words you want in the passphrase: "))
 if __name__ == '__main__':
-    print(generate_passphrase(7))
-    print(generate_passphrase(7))
+    print(f'Generated passphrase is: {generate_passphrase(n)} ')
